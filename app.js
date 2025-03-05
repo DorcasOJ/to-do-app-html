29fbfd2c07d91cd668a3a073d7b0f128ab09d536
Dorcas <ayanfedorcas2017@gmail.com>
2025-03-05 06:41:52 +0100

    update1

--- app.js
+++ app.js
@@ -1,6 +1,62 @@
 
 
 
+// document.getElementById('add').addEventListener('click', addTask);
+// document.getElementById('input').addEventListener('keydown', function (event) {
+//     event.key === 'Enter' ? addTask() : null;
+// } );
+
+// function addTask() {
+//     let task = document.getElementById('input');
+//     if (task.value) {
+//         const listContainer = document.getElementsByClassName("list-container")[0]
+//         const listItem = document.createElement('li')
+//         listItem.innerHTML = task.value
+
+//         const span = document.createElement('span')
+//         span.className = 'extend'
+//         span.innerHTML = '>'
+        
+//          const span2 = document.createElement('span')
+//         span2.className="completed"
+//         span2.innerHTML = 'x'
+       
+//         const span3 = document.createElement('span')
+//         // span3.className = '
+//         const italic = document.createElement('i')
+//         italic.innerHTML = 'Calender'
+//         const italic2 = document.createElement('i')
+//         italic.innerHTML = 'List'
+//         const italic3 = document.createElement('i')
+//         italic.innerHTML = 'x'
+//         span3.appendChild(italic)
+//         span3.appendChild(italic2)
+//         span3.appendChild(italic3)
+
+//         listItem.addEventListener('click', () => listItem.classList.toggle('checked'))
+
+//         span2.addEventListener('click', () => {
+//             listContainer.removeChild(listItem)
+//         })
+//         // span.addEventListener('click', () => {
+//         //     span3.classList.toggle('extended')
+//         // })
+//         listItem.appendChild(span)
+//         listItem.appendChild(span2)
+//         listItem.appendChild(span3)
+        
+//         listContainer.appendChild(listItem)
+//     } else if (task.value === "") {
+//         alert('Please enter a task');
+//     }
+//     const items = document.querySelectorAll('.list-container>li')
+//     task.value = "" 
+//     console.log(items)
+// }
+// document.querySelectorAll('.list-container>li').forEach(item => {
+//     item.addEventListener('click', () => item.classList.toggle('checked'))
+// })
+
 document.getElementById('add').addEventListener('click', addTask);
 document.getElementById('input').addEventListener('keydown', function (event) {
     event.key === 'Enter' ? addTask() : null;
@@ -11,44 +67,88 @@ function addTask() {
     if (task.value) {
         const listContainer = document.getElementsByClassName("list-container")[0]
         const listItem = document.createElement('li')
-        listItem.innerHTML = task.value
-
+    
         const span = document.createElement('span')
-        span.className = 'extend'
-        span.innerHTML = '>'
-        
-         const span2 = document.createElement('span')
+        span.innerHTML = task.value
+
+        const span1 = document.createElement('span')
+        span1.className = 'edit'
+        const inputEdit = document.createElement('input')
+        inputEdit.type = 'text'
+        const buttonEdit = document.createElement('button')
+        buttonEdit.innerHTML = 'save'
+        span1.appendChild(inputEdit)
+        span1.appendChild(buttonEdit)
+
+        const span2 = document.createElement('span')
         span2.className="completed"
         span2.innerHTML = 'x'
        
         const span3 = document.createElement('span')
-        // span3.className = '
+        span3.className = 'edit-button'
         const italic = document.createElement('i')
-        italic.innerHTML = 'Calender'
-        const italic2 = document.createElement('i')
-        italic.innerHTML = 'List'
-        const italic3 = document.createElement('i')
-        italic.innerHTML = 'x'
-        span3.appendChild(italic, italic2, italic3)
+        italic.innerHTML = 'edit'
+        span3.appendChild(italic)
 
-        listItem.addEventListener('click', () => listItem.classList.toggle('checked'))
-        
+        listItem.appendChild(span)
+        listItem.appendChild(span1)
+        listItem.appendChild(span2)
+        listItem.appendChild(span3)
+        listContainer.appendChild(listItem)
+
+        span.addEventListener('click', () => {
+            span.classList.toggle('checked'); 
+            saveData()
+        })
+
         span2.addEventListener('click', () => {
             listContainer.removeChild(listItem)
+            saveData()
         })
-        span.addEventListener('click', () => {
-            span3.classList.toggle('extended')
+        span3.addEventListener('click', () => {
+          
+            
+            let editInput = document.querySelector('.edit')
+            editInput.style.display = "flex"
+            editInput.querySelector('input').value = span.innerHTML
+            span.innerHTML = 'editing...'
+                span3.style.display = "none"
+            editInput.querySelector('input').addEventListener('keydown', function (event) {
+                if (event.key === "Enter") {
+                    span.innerHTML = editInput.querySelector('input').value
+                    editInput.style.display = "none"
+                    span3.style.display = "flex"
+                }
+                saveData()
+            })
+            editInput.querySelector('button').addEventListener('click', function () {
+                if (editInput.querySelector('input').value) {
+                    span.innerHTML = editInput.querySelector('input').value
+                    editInput.style.display = "none"
+                    span3.style.display = "flex"
+                }
+                saveData()
+            })
         })
-        listItem.appendChild(span, span2, span3)
-        
-        listContainer.appendChild(listItem)
+        task.value = ""
+        saveData()
+       
     } else if (task.value === "") {
         alert('Please enter a task');
     }
-    const items = document.querySelectorAll('.list-container>li')
-    task.value = "" 
-    console.log(items)
+   
 }
-document.querySelectorAll('.list-container>li').forEach(item => {
-    item.addEventListener('click', () => item.classList.toggle('checked'))
-})
+
+function saveData () {
+    // localStorage.setItem('data', JSON.stringify(document.getElementsByClassName('list-container')[0].innerHTML))
+    localStorage.setItem('data', document.getElementsByClassName('list-container')[0].innerHTML)
+    console.log(localStorage.getItem('tasks'))
+}
+
+function showTask () {
+    if (localStorage.getItem('data')) {
+        document.getElementsByClassName('list-container')[0].innerHTML = localStorage.getItem('data')
+    }
+}
+
+showTask()
